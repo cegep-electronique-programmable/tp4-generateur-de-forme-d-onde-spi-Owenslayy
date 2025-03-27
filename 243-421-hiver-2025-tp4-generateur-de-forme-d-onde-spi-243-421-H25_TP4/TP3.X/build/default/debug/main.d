186911caf5b0e857d6cc6ab66a12282f589d6750
Owenslayy <e2288053@cmaisonneuve.qc.ca>
2025-03-27 14:55:24 -0400

    Add files via upload

---
+++ 243-421-hiver-2025-tp4-generateur-de-forme-d-onde-spi-243-421-H25_TP4/TP3.X/build/default/debug/main.d
@@ -0,0 +1,4 @@
+build/default/debug\main.i: main.c mcc_generated_files/mcc.h \
+  mcc_generated_files/device_config.h mcc_generated_files/pin_manager.h \
+  mcc_generated_files/interrupt_manager.h mcc_generated_files/spi.h \
+  mcc_generated_files/tmr1.h mcc_generated_files/eusart1.h
